e78369923eb249835b124bd902a69cd04b9d3e38
Renan Gonçalves Pereira <renan4573@hotmail.com>
2017-10-07 10:02:55 -0300

    Update insertionsort.c
    Ajustes

--- Bublesort x Insertionsort/insertionsort.c
+++ Bublesort x Insertionsort/insertionsort.c
@@ -1,73 +1,73 @@
-/* funcao insertionsort com vetor defindo em tempo de execução e números aleatórios de 1 à 100
+/* funcao Insertion Sort com vetor defindo em tempo de execução e com números aleatórios de 1 à 100000
 obs : funçâo mostra a quantidade de trocas realizadas*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int *alocavetor (int n);
-void insertionsort (int *v,int n);
-void troca (int *v, int i, int j);
-void mostra (int *v, int n);
+int *Alocavetor (int n);
+void InsertionSort (int *v,int n);
+void Troca (int *v, int i, int j);
+void Print (int *v, int n);
 
 int main (void) {
-    
+
     int *A, n;
-    
+
     printf("Digite o tamanho do vetor:\n");
     scanf("%d", &n);
 
-    A = alocavetor(n); 
-
-    insertionsort(A,n);	
-    mostra(A,n);
+    A = Alocavetor(n);
+    InsertionSort(A,n);
+    Print(A,n);
 
     free(A);
-
     A = NULL;
 
     return 0;
 
 }
 
-int *alocavetor (int n) {
+int *Alocavetor (int n) {
 
   int *x, i = 0;
-  
+
   x = (int*) calloc(n, sizeof(int));
 
   if (x != NULL) {
-
+    //semente de rand
     srand(time(NULL));
 
     while (i < n) {
 
-      x[i] = rand() % 100;
+      x[i] = rand() % 100000;//função que gera números aleatórios
       i++;
+
     }
-  }
-  
+  } else printf("Impossivel alocar o espaço requisitado!\n");
+
   return x;
 }
 
-void insertionsort (int *v, int n) {
-   
-  int i,j,trocas = 0;
+void InsertionSort (int *v, int n) {
+
+  int i,j;
+  double trocas = 0;
 
   for (i = 1; i < n; i++) {
 
   	j = i;
 
   	while (j > 0 && v[j] < v[j-1]) {
-  		troca (v, j, j-1);
-      trocas++;
+  		Troca (v, j, j-1);
+        trocas++;
         j--;
     }
-  }	
-  printf("\nQuantidade de trocas : %d\n", trocas);
+  }
+  printf("Quantidade de trocas : %.lf\n", trocas);
 }
 
-void troca (int *v, int i, int j) {
+void Troca (int *v, int i, int j) {
 
 	int aux;
 
@@ -77,14 +77,14 @@ void troca (int *v, int i, int j) {
 
 }
 
-void mostra (int v[], int n) {
+void Print (int v[], int n) {
 
 	int i = 0;
 
 	while (i < n) {
-     
+
      printf("%d ", v[i]);
-     
+
      i++;
 
 	}
