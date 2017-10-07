50ba7d49caffad232f7e6d64dc828ad32c78c468
Renan Gonçalves Pereira <renan4573@hotmail.com>
2017-10-07 10:09:23 -0300

    Update bublesort.c
    ajustes

--- Bublesort x Insertionsort/bublesort.c
+++ Bublesort x Insertionsort/bublesort.c
@@ -1,65 +1,67 @@
-/* funcao bublesort com vetor defindo em tempo de execução e números aleatórios de 1 à 100
+/* funcao Bubble Sort com vetor defindo em tempo de execução e com números aleatórios de 1 à 100000
 obs : funçâo mostra a quantidade de trocas realizadas*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int *alocavetor (int n);
-void bublesort (int *v, int n);
-void print (int *v, int n);
-void troca (int *v, int i, int j); 
+int *Alocavetor (int n);
+void BubbleSort (int *v, int n);
+void Print (int *v, int n);
+void Troca (int *v, int i, int j);
 
 int main (void) {
-	
+
 	int *A, n;
 
 	printf("Digite o tamanho do vetor:\n");
 	scanf("%d", &n);
 
-	A = alocavetor(n);
+	A = Alocavetor(n);
 
-	bublesort(A, n);
-	print(A, n);
-	
-	free(A)
-	A = NULL;	
+	BubbleSort(A, n);
+	Print(A, n);
+
+	free(A);
+	A = NULL;
 
 	return 0;
 }
 
-int *alocavetor (int n) {
+int *Alocavetor (int n) {
 
 	int *x, i = 0;
-    
+
 	x = (int*) calloc(n, sizeof(int));
 
 	if (x != NULL) {
 
+        // semente de rand para gerar números aleatórios
 		srand(time(NULL));
 
 		while (i < n) {
 
-			x[i] = rand() % 100;
+			x[i] = rand() % 100000;// geração de números aleatórios
 
 			i++;
 		}
-	}
+	} else printf("Impossível alocar espaço requisitado!\n");
 
     return x;
 }
 
 
-void bublesort (int *v, int n) {
+void BubbleSort (int *v, int n) {
 
-	int i = 0 ,trocas = 0;
+	int i = 0;
+	double trocas = 0;
 
 	while (n > 1) {
 
 		for (i = 0; i < n-1; i++) {
 
 		     if (v[i] > v[i+1]) {
-			     troca(v, i, i+1);
+			     Troca(v, i, i+1);
 			     trocas++;
 		     }
 
@@ -67,10 +69,10 @@ void bublesort (int *v, int n) {
     n--;
 	}
 
-	printf("Quantidade de trocas = %d\n", trocas);
-}	
+	printf("Quantidade de trocas = %.lf\n", trocas);
+}
 
-void troca (int *v, int i, int j) {
+void Troca (int *v, int i, int j) {
 
     int aux = v[i];
     v[i] = v[j];
@@ -78,7 +80,7 @@ void troca (int *v, int i, int j) {
 
 }
 
-void print (int *v, int n) {
+void Print (int *v, int n) {
 
 	int i = 0;
 
